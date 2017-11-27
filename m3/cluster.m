2f53069329cab9d764dce9b945de1364d006e547
Vlad104 <vlad104khk@gmail.com>
2017-11-27 17:49:13 +0300

    Add files via upload

--- m3/cluster.m
+++ m3/cluster.m
@@ -18,59 +18,62 @@ D(3,2) = 1;
 D(4,2) = 1;
 D(4,1) = 1;
 D(4,3) = 1;
+%D(3,4) = 1;
 
 B = A.*D;
 F(1:int32(EQ64*EQ256/2),1) = 0;
 H(1:EQ64,1:EQ256) = 0;
-K(1:4,1:4) = 0;
-K(1,1:2) = 1;
+K(1:EQ256/4*EQ64/4,1:2) = 0;
 n = 1;
-hn = 1;
-hnn = 0;
-hni = 1;
-
-flag = 1;
 BB = B;
-
 for ii = 1 : EQ64
 	for jj = 1 : EQ256 %#ok<ALIGN>
-        for hni = 1 : hn
-            i = K(hni,1);
-            j = K(hni,2); %нужно решить проблему рекурсии. Запомнитиь 4 точки. Затем встать в одну из них и запомнить новые
-            if ( j <= EQ256 && G(i,j) == 0 && D(i,j) > 0 )
+        flag = 0;
+        hnn = 1; 
+        while ( hnn > 0 && D(ii,jj) > 0 ) %&& G(ii,jj) == 0 %(~flag || hnn > 0)
+            if ~flag %if first times in while
+                i = ii;
+                j = jj; 
+                flag = 1;
+            else %if not first times in while
+                i = K(hnn,1);
+                j = K(hnn,2);
+            end;
+
+            if ( j <= EQ256 && i <= EQ64 && G(i,j) == 0 && D(i,j) > 0 )
             	F(n,1) = F(n,1) + 1;
             	G(i,j) = 1;
             	H(i,j) = 0;
-            	BB(i,j) = 2;
-                hnn = 0;
-            	if ( j + 1 <= EQ256 && G(i, j+1) == 0 && D(i, j+1) > 0 )
+            	BB(i,j) = 2;                
+                hnn = hnn - 1;
+                
+            	if ( j + 1 <= EQ256 && G(i, j+1) == 0 && D(i, j+1) > 0 ) %#ok<ALIGN>
                 	H(i,j+1) = 1;
-                    hnn = hnn + 1; 
+                    hnn = hnn + 1;
                     K(hnn,1) = i;
-                    K(hnn,2) = j;
+                    K(hnn,2) = j+1;
                 end;
 
                 if ( j - 1 >= 1 && G(i, j-1) == 0 && D(i, j-1) > 0 )
                 	H(i,j-1) = 1;
                     hnn = hnn + 1; 
                     K(hnn,1) = i;
-                    K(hnn,2) = j;
+                    K(hnn,2) = j-1;
                 end;
 
                 if ( i + 1 <= EQ64 && G(i+1, j) == 0 && D(i+1, j) > 0 )
                 	H(i+1,j) = 1;
                     hnn = hnn + 1; 
-                    K(hnn,1) = i;
+                    K(hnn,1) = i+1;
                     K(hnn,2) = j;
                 end;
 
                 if ( i - 1 >= 1 && G(i-1, j) == 0 && D(i-1, j) > 0 )
                 	H(i-1,j) = 1;
                     hnn = hnn + 1; 
-                    K(hnn,1) = i;
+                    K(hnn,1) = i-1;
                     K(hnn,2) = j;
                	end;
-                hn = hnn;
             end;
         end;
         if (F(n,1) > 0)
